3c756a4b689c09fd17c9f18c393f097bb01e2ad2
Enoch-Kim <enochkim1220@gmail.com>
2021-02-19 16:37:53 +0900

    Code Refactoring (BluesPresenter) * give unity * divide function

--- highballuos/src/js/blues/BluesPresenter.js
+++ highballuos/src/js/blues/BluesPresenter.js
@@ -15,7 +15,22 @@ const BluesPresenter = (function(){
         instance = this;
     }
 
-    // Button
+    // --- Button Code ---
+
+    function _addEventsOfBluesBtn() {
+        this._view.addBtnHandler("mouseover", _onMouseoverBtn.bind(this));
+        this._view.addBtnHandler("mouseout", _onMouseoutBtn.bind(this));
+        this._view.addBtnHandler("click", _onClickBtn.bind(this));
+    }
+
+    function _removeEventsOfBluesBtn() {
+        this._view.removeBtnHandler("mouseover", _onMouseoverBtn);
+        this._view.removeBtnHandler("mouseout", _onMouseoutBtn);
+        this._view.removeBtnHandler("click", _onClickBtn);
+    }
+
+    // click event func of Blues Button
+    // if text's status == hate -> replace text to styled one
     function _onClickBtn(){
         if(this._model.getStatus() != BTN_MODE.HATE_MODE) return;
         this._model.setText(this._model.getStyledText());
@@ -27,21 +42,23 @@ const BluesPresenter = (function(){
         this._view.setStyledTextDisplay(false);
     }
 
-    function _addMouseEventOnBtn(){
-        this._view.addBtnHandler("mouseover", function(){
-            if(this._model.getStatus() == BTN_MODE.HATE_MODE)
-                this._view.setStyledTextDisplay(true);
-        }.bind(this));
-
-        this._view.addBtnHandler("mouseout", function(){
-            this._view.setStyledTextDisplay(false);
-        }.bind(this));
+    // mouseover func of Blues Button
+    // if text's status == hate -> display styled text view
+    function _onMouseoverBtn() {
+        if(this._model.getStatus() == BTN_MODE.HATE_MODE)
+            this._view.setStyledTextDisplay(true);
     }
 
+    // mouseout func of Blues Button
+    // hide styled text view
+    function _onMouseoutBtn() {
+        this._view.setStyledTextDisplay(false);
+    }
 
-    // TextArea
+    // --- Text Code ---
+
+    // on text change -> timer reset & hide styled text view & detect when text !empty
     function _onTextChange(){
-
         if(this._timer) clearTimeout(this._timer);
 
         const text = this._view.getText();
@@ -51,15 +68,16 @@ const BluesPresenter = (function(){
 
         this._view.setBtnMode(BTN_MODE.LOADING_MODE);
         this._model.setText(text);
-        // debouncing 1000ms
-        this._timer = setTimeout(_detectHateSpeech.bind(this), 1000);
+        this._timer = setTimeout(_detectHateSpeech.bind(this), 1000); // debouncing 1000ms
     }
     
+    // not completed need tensorflow js model of styling text
     function _stylingText(){
         this._model.setStyledText(this._model.getText().split("").reverse().join(""));
         this._view.setStyledText(this._model.getStyledText());
     }
 
+    // not completed need tensorflow js model of detect hate speech
     function _detectHateSpeech(){
         // const text = this._model.getText();
         if(Math.random() >= 0.5){
@@ -73,7 +91,20 @@ const BluesPresenter = (function(){
     }
 
     
-    // Container Code
+    // --- Target & Container Code ---
+
+    // add Target Hander
+    function _addEventsOfTarget() {
+        eventListenerHelper(document.body, "focusin", _onFocusIn.bind(this), true);
+        eventListenerHelper(window, "resize", _onResize.bind(this), true);
+    }
+
+    function _removeEventsOfTarget() {
+        eventListenerHelper(document.body, "focusin", _onFocusIn.bind(this), false);
+        eventListenerHelper(window, "resize", _onResize.bind(this), false);
+    }
+
+    // only textarea of line more than 1 && editable div
     function _isTextElement(element){
         switch(element.tagName) {
             case TEXTAREA_TAG :
@@ -88,22 +119,30 @@ const BluesPresenter = (function(){
         }
     }
 
+    // focusin event func of target textarea
+    // if focused component is text area && target changed
+    // -> remove exist target event handler
+    // -> change target & add event handler
     function _onFocusIn(event){
         event.stopPropagation();
 
         if(_isTextElement(event.path[0]) && event.path[0] !== this._view.getTargetView()){
             this._view.removeTargetHandler("input", _onTextChange);
+            
             this._view.setTarget(event.path[0]);
             this._view.setBtnMode(BTN_MODE.NORMAL_MODE);
             this._view.addTargetHandler("input", _onTextChange.bind(this));
             const text = this._view.getText();
+            this._model.setText(text ? text : "");
+
             if(text && text != ""){
-                this._model.setText(this._view.getText());
                 _onTextChange.bind(this)();
             }
         }
     }
 
+    // resize event func of target textarea
+    // if resize -> copy layout to Blues Container
     function _onResize(event){
         event.stopPropagation();
         if(this._view.getTargetView()){
@@ -123,21 +162,20 @@ const BluesPresenter = (function(){
             if(this._model.getIsTurnOn()) return;
 
             this._model.setIsTurnOn(true);
+            _addEventsOfBluesBtn.bind(this)();
+            _addEventsOfTarget.bind(this)();
             this._view.display();
-            _addMouseEventOnBtn.bind(this)();
-            eventListenerHelper(document.body, "focusin", _onFocusIn.bind(this), true);
-            eventListenerHelper(window, "resize", _onResize.bind(this), true);
-            this._view.addBtnHandler("click", _onClickBtn.bind(this));
         },
         off : function(){
             if(!this._model.getIsTurnOn()) return;
 
             this._model.setIsTurnOn(false);
-            eventListenerHelper(document.body, "focusin", _onFocusIn.bind(this), false);
-            eventListenerHelper(window, "resize", _onResize.bind(this), false);
-            if(this._view.hasTarget()){
+            _removeEventsOfTarget.bind(this)();
+            _removeEventsOfBluesBtn.bind(this)();
+
+            if(this._view.hasTarget())
                 this._view.removeTargetHandler("input", _onTextChange);
-            }
+            
             this._view.remove();
         }
     }
