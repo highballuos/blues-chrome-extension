176a30cdb7029ad445c70db2db3315b557f10ca5
Enoch-Kim <enochkim1220@gmail.com>
2021-04-01 01:38:44 +0900

    Add gitignore contents and remove useless files

--- test/webpack.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-module.exports = {
-    entry: "./test.js",
-    output: {
-        filename: "index.js",
-        path: path.resolve(__dirname, "./"),
-    }
-}
--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-module.exports = {
-    entry: "./src/background/detect.js",
-    output: {
-        filename: "test.js",
-        path: path.resolve(__dirname, "./"),
-    }
-}
